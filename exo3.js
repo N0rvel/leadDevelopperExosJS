55a01e36f8b720b13df05ff8eb797b40c84e42d5
Bastien <bastienreturn@outlook.fr>
2021-02-22 15:26:57 +0100

    [EXO3] - Ajout de l'exercice 3

---
+++ exo3.js
@@ -0,0 +1,24 @@
+const scanf = require('scanf');
+
+console.log('Saisir 5 nombres');
+
+let tableau = [];
+for (let i = 0; i < 5 ; i++){
+    tableau.push(scanf("%d"));
+}
+
+let previousItem = tableau.indexOf(0);
+let indiceGrandNombre = 0;
+let moy = 0;
+tableau.forEach(function(item, index){
+    if (previousItem<item && tableau[indiceGrandNombre] < item){
+        indiceGrandNombre = index;
+        console.log("ici " + item)
+    }
+    previousItem = item;
+    moy += item;
+});
+moy /= tableau.length;
+console.log(tableau);
+console.log("Indice du plus grand nombre: %s", indiceGrandNombre);
+console.log("Moyenne: %s", moy);
