81f455190ef223ddba73995e86d5d991c6d59161
Bastien <bastienreturn@outlook.fr>
2021-02-22 14:17:11 +0100

    [EXO1] - Init projet + ajout fichier exo1

---
+++ exo1.js
@@ -0,0 +1,13 @@
+var scanf = require('scanf');
+
+console.log('Saisir une chaine de caractère');
+var chaine1 = scanf('%s');
+
+console.log('Saisir une deuxième chaine de caractère');
+var chaine2 = scanf('%s');
+
+if (chaine1.length === 2 * chaine2.length){
+    console.log("La chaine 1 (longueur: %s) est deux fois plus longue que la chaine 2 (longueur: %s)", chaine1.length, chaine2.length)
+} else {
+    console.log("La chaine 1 (longueur: %s) n'est pas deux fois plus longue que la chaine 2 (longueur: %s)", chaine1.length, chaine2.length)
+}
