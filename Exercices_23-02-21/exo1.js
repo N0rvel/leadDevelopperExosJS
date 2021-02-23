0d48a0f952ea3724b26117cc803ef72d764555c0
N0rvel <bastienreturn@outlook.fr>
2021-02-23 14:58:30 +0100

    [Exercice DOM] - Indentation

--- Exercices_23-02-21/exo1.js
+++ Exercices_23-02-21/exo1.js
@@ -1,7 +1,7 @@
-    let tab = ["Mot1", "Mot2", "Mot3", "Mot4"];
-    let currentWord = tab[0];
-    let indice = 0;
-    document.getElementById("button").onclick = function(){
+let tab = ["Mot1", "Mot2", "Mot3", "Mot4"];
+let currentWord = tab[0];
+let indice = 0;
+document.getElementById("button").onclick = function(){
     let x = Math.floor(Math.random() * 256);
     let y = Math.floor(Math.random() * 256);
     let z = Math.floor(Math.random() * 256);
@@ -10,10 +10,11 @@
     p.innerHTML += currentWord;
 }
 
-    window.setInterval(function(){
+window.setInterval(function(){
     if (indice > tab.length-1){
     indice = 0;
-}
+    }
     currentWord = tab[indice]
     indice++;
 }, 5000);
+
